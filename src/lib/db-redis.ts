aa926bebd792bf3fa7c180893229f25c5dc667a0
Sachin Sadiwal <sachindeveloper.ch@gmail.com>
2025-08-21 21:04:17 +0530

    updated code

--- src/lib/db-redis.ts
+++ src/lib/db-redis.ts
@@ -1,5 +1,5 @@
 import Redis from "ioredis"
 
-const redis = new Redis("rediss://red-cvicmm3uibrs73fqovqg:uNFzyUMYAQLA0Pok4U55bZiG70VaCEw5@oregon-keyvalue.render.com:6379")
+const redis = new Redis(process.env.REDIS_URI!)
 
 export default redis
